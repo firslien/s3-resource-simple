51a5450168cd6936e98f79c9f898e02af53121ad
sam <shan.gao@qihaixing.com>
2022-09-21 14:16:20 +0800

    version by ci_version file

--- assets/emit.sh
+++ assets/emit.sh
@@ -2,7 +2,15 @@
 
 set -e
 
+dir=$(cat ci_version)
+
+prefix_params=$(echo $dir| sed 's/\//%2f/g')%2f
+
 # give back a(n empty) version, so that the check passes when using `in`/`out`
 echo "{
-  \"version\": {}
+  \"version\": { \"path\": \"$dir\" },
+  \"metadata\": [  
+    { \"name\": \"path\", \"value\": \"$dir\" },
+    { \"name\": \"url\", \"value\": \"$url/buckets/$bucket?prefix=$prefix_params\" } 
+  ]
 }"
